a78f1c7cd1b7849e7d409644e2a205ebfba01cc8
E Cussler <cusslere@gmail.com>
2022-11-12 07:58:55 -0800

    Added save to local storage framework, needs debugging

--- Assets/script.js
+++ Assets/script.js
@@ -9,6 +9,28 @@ $(function () {
   let currentTime = dayjs().format('h:mm'); 
   let currentHour = dayjs().get('h'); 
 
+  // FUNCTION TO DISPLAY CURRENT DATE/TIME AT TOP OF PAGE
+  var date = dayjs().format('dddd, MMMM D, YYYY  h:mm a');
+  $('#currentDay').text(date);
+  setBlockColor(); 
+
+
+  // FUNCTION SETS BLOCK COLOR FOR PAST, PRESENT, FUTURE
+
+  function setBlockColor() {
+    timeBlocks.each(function(i) {
+      let blockId = this.id; 
+      console.log(+blockId, currentHour); 
+      // If statement uses time block ID (in 24hr) to determine which color to use
+      if (currentHour > +blockId) {
+        this.classList.add('past'); 
+      } else if (currentHour === +blockId) {
+        this.classList.add('present');         
+      } else {
+        this.classList.add('future'); 
+      }
+    }); 
+  };
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -17,34 +39,33 @@ $(function () {
   // useful when saving the description in local storage?
   //
   
-  saveBtns.on('click', function () { //put saveProject function here later
-    console.log('save button clicked');
-    console.log(currentTime);  
-    console.log(currentHour);  
-  })
+  saveBtns.on('click', function (event) { 
+    event.preventDefault(); 
+    let target = $(event.target); 
 
-  // function saveProject(event) {
-  //   event.preventDefault(); 
+    let projectObj = { //create a larger array for all hours in the program so that updates are just updating the correct hour and not overriding each one
+      blockId: target.parent.id, 
+      time: target.siblings('div').text, 
+      project: target.siblings('textarea')
+    };
 
-  //   const projectObj { //create a larger array for all hours in the program so that updates are just updating the correct hour and not overriding each one
-  //     block: event.target.parent(); 
-  //     time: event.target.siblings('div').text; 
-  //     project: event.target.siblings('textarea'); 
-  //   }
+    console.log(projectObj); 
 
-  //   let timeArray = []; 
-  //   timeArray.push(projectObj);
-  //   let storageManager = localStorage.getItem('storageManager'); 
-  //     if (!storageManager) {
-  //       return;
-  //     }
-  //   timeArray = JSON.parse(storageManager); 
-  //   for (let i=0; i < timeArray.length; i++) {
+    let timeArray = []; 
+    timeArray.push(projectObj);
+    let storageManager = localStorage.getItem('storageManager'); 
+      if (!storageManager) {
+        return;
+      }
+    timeArray = JSON.parse(storageManager); 
+    for (let i=0; i < timeArray.length; i++) {
 
-  //   }
-  //   timeArray.push(storageManager); 
-  //   localStorage.setItem('storageManager', JSON.stringify(timeArray)); 
-  // }
+    }
+    timeArray.push(storageManager); 
+    localStorage.setItem('storageManager', JSON.stringify(timeArray)); 
+    });
+
+   
 
   // Get is so when you click the save button it traverses the DOM to get the text value and hours saved
   // Can add data attributes
@@ -52,26 +73,6 @@ $(function () {
   // Use JSON.stringify 
 
 
-  function setBlockColor() {
-      timeBlocks.each(function(i) {
-        let blockId = this.id; 
-        console.log(+blockId, currentHour); 
-        if (currentHour > +blockId) {
-          this.classList.add('past'); 
-        } else if (currentHour === +blockId) {
-          this.classList.add('present');         
-        } else {
-          this.classList.add('future'); 
-        }
-      }); 
-  };
-
-
-
-  // function getTime() {
-  //   let currentTime = dayjs().format('h:mm'); 
-  //   console.log(currentTime); 
-  // }
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
@@ -80,9 +81,6 @@ $(function () {
 
 
 
-  // TODO: Add code to display the current date in the header of the page.
-  var date = dayjs().format('dddd, MMMM D, YYYY  h:mm a');
-  $('#currentDay').text(date);
-  setBlockColor(); 
+
 
 });
